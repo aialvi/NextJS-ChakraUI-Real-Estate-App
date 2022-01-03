749352fba48b9f12c0224d3bd6fedd8b31619ebb
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-01-03 13:32:57 +0600

    Modify Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,79 @@
 import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
+
+const Banner = ({
+  imageUrl,
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  linkName,
+  buttonText,
+}) => {
+  return (
+    <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+      <Image src={imageUrl} width="500" height="300" alt="banner" />
+      <Box p="5">
+        <Text color={"gray.500"} fontSize="sm" fontWeight="medium">
+          {purpose}
+        </Text>
+        <Text fontSize="3xl" fontWeight="bold">
+          {title1} <br /> {title2}
+        </Text>
+        <Text
+          fontSize="lg"
+          paddingTop="3"
+          paddingBottom={"3"}
+          color={"gray.700"}
+          fontWeight="medium"
+        >
+          {desc1} <br /> {desc2}
+        </Text>
+        <Button fontSize="xl">
+          <Link href={linkName}>
+            <a>{buttonText}</a>
+          </Link>
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Next Real Estate</title>
         <meta name="description" content="Get the 'Real' deals at ease" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Box>
+        <Banner
+          imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+          purpose="RENT A HOME"
+          title1="Rental Homes for"
+          title2="Everyone"
+          desc1="Explore apartments, villas, houses"
+          desc2="and more"
+          linkName="/search?purpose=for-rent"
+          buttonText="Explore Renting"
+        />
+        <Flex flexWrap="wrap" m="10"></Flex>
+        <Banner
+          imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+          purpose="BUY A HOME"
+          title1="Find, Buy and Own Your"
+          title2="Dream Home"
+          desc1="Explore apartments, villas, houses"
+          desc2="and more"
+          linkName="/search?purpose=for-rent"
+          buttonText="Explore Buying"
+        />
+        <Flex flexWrap="wrap" m="10"></Flex>
+      </Box>
     </div>
   );
 }
