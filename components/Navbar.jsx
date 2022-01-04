422429f81f20c3e46a27684054df1546e3612c0b
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-01-04 14:46:41 +0600

    Add padding for logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,7 +15,7 @@ import { FiKey } from "react-icons/fi";
 
 const Navbar = () => (
   <Flex p="2" borderBottom="1px" borderColor="gray.100">
-    <Box fontSize="3xl" color="blue.400" fontWeight="bold">
+    <Box fontSize="3xl" color="blue.400" fontWeight="bold" paddingLeft={1}>
       <Link href="/" paddingLeft="2">
         TriaHomes
       </Link>
