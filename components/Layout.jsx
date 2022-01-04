798e0a9ae13bd88cb509d4eccd35162d63c58514
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-01-04 13:34:36 +0600

    Modify imports in Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 import Footer from "./Footer";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Real Estate</title>
+        <title>TriaHomes - One Stop Real Estate Solutioon</title>
       </Head>
       <Box maxWidth="1280px" m="auto">
         <header>
